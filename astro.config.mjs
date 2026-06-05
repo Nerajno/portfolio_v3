ad275828858fcf4905904884f68c6f694b4368f7
Nerando Johnson <nerajno@gmail.com>
2026-06-05 19:33:01 -0400

    restarted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
@@ -17,7 +16,6 @@ export default defineConfig({
   adapter: netlify(),
   integrations: [
     react(),
-    tailwind(),
     mdx({
       syntaxHighlight: false,
       remarkPlugins: [],
